073a58a85e4899ed8a03433ff493486b6fcf02db
albim72 <marcin.albiniak72@gmail.com>
2023-12-27 16:00:34 +0100

    Update bookstore.py

--- DZIEN_1/bookstore.py
+++ DZIEN_1/bookstore.py
@@ -28,7 +28,34 @@ class Firma:
         self.nazwa = nazwa
         self.branza = branza
         self.miasto = miasto
-        
+
     def show_firma(self):
         print(f'firma: {self.nazwa}({self.branza}) - miasto: {self.miasto}')
-        
+
+
+class Pracownik(Osoba,Firma):
+
+    def __init__(self, imie, wiek, waga, wzrost,nazwa,branza,miasto,stanowisko,wynagrodzenie):
+        Osoba.__init__(self,imie, wiek, waga, wzrost)
+        Firma.__init__(self,nazwa,branza,miasto)
+        self.stanowisko = stanowisko
+        self.wynagrodzenie = wynagrodzenie
+
+
+    def dane_pracownika(self):
+        print(f'stanowisko: {self.stanowisko}, wynagrodzenie: {self.wynagrodzenie} zł')
+
+    def czypracownik(self):
+        return True
+
+
+pr1 = Pracownik("Olga",32,56,167,"ABC","IT",
+                "Kraków","Dyrektor",11200)
+pr1.print_osoba()
+pr1.dane_pracownika()
+pr1.show_firma()
+
+print(f"czy osoba jest pracownikiem?({pr1.czypracownik()})")
+
+
+
