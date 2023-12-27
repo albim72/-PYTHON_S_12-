fb362501ed6bdd644fdc1f67582f78eae78b1f81
albim72 <marcin.albiniak72@gmail.com>
2023-12-27 10:15:04 +0100

    Create testowy.py

---
+++ DZIEN_1/testowy.py
@@ -0,0 +1,12 @@
+print("to jest test")
+a = 87
+print("wartość a = " + str(a))
+print(f"wyświetlenie a za pomocą f-string -> a = {a}")
+
+print(type(a))
+b:int = 101
+print(b,type(b))
+b = True
+print(b,type(b))
+b = "prawda"
+print(b,type(b))
