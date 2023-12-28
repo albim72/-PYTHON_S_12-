d167a0dfa469e882c95f3648b747837cd4f0ffdc
albim72 <marcin.albiniak72@gmail.com>
2023-12-28 10:17:27 +0100

    Create main.py

---
+++ DZIEN_2/moje_moduly/main.py
@@ -0,0 +1,8 @@
+# import dane
+# import dane as dn
+from dane import nrfilii as filia
+from dane import book as bk
+
+print("________________ wywietalnie bezpośrednio ____________________")
+print(filia)
+print(bk)
