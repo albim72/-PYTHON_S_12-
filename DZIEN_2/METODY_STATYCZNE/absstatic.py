ab2721f55dc7034bf3cee16f18f4af267b492034
albim72 <marcin.albiniak72@gmail.com>
2023-12-28 16:16:17 +0100

    Create absstatic.py

---
+++ DZIEN_2/METODY_STATYCZNE/absstatic.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+class Tekst(ABC):
+    
+    def __init__(self,t):
+        self.t=t
+        
+    @abstractmethod
+    @staticmethod
+    def opis(info):pass
+    
+    @abstractmethod
+    def msg(self):pass
