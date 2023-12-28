4d9939c0b98537a703b52cfad8c396bf7515c4a3
albim72 <marcin.albiniak72@gmail.com>
2023-12-28 09:03:50 +0100

    Update funkcja_w_funkcji.py

--- DZIEN_1/funkcja_w_funkcji.py
+++ DZIEN_1/funkcja_w_funkcji.py
@@ -1,4 +1,6 @@
 #przykład 1
+import math
+
 
 def witaj(imie):
     return f"dziękujemy za założenie konta: {imie}"
@@ -36,8 +38,8 @@ def rejestracja(oplata):
     def lista_zawodnikow(nr):
         return f"jesteś na liście zawodników nr {nr}- opłata wniesiona!"
 
-    def brak():
-        return "brak wpłaty - masz 3 dni na uzupełnienie!"
+    def brak(rodzaj):
+        return f"brak wpłaty, rodzaj - {rodzaj}- masz 3 dni na uzupełnienie!"
 
     def error():
         return "błąd w rejestracji: 1 - opłacono, 2 - nieopłacono, inna wartość -> błąd!"
@@ -49,7 +51,63 @@ def rejestracja(oplata):
     else:
         return error
 
-
 print(rejestracja(1)(56))
-print(rejestracja(0)())
+print(rejestracja(0)("karta maestro"))
 print(rejestracja(56)())
+
+
+#przykład 3
+
+def startstop(funkcja):
+    def wrapper(*args):
+        print("początek procesu...")
+        funkcja(*args)
+        print("koniec procesu...")
+    return wrapper
+
+def zawijanie(czego):
+    print(f"zawijanie {czego} w sreberka")
+
+zw = startstop(zawijanie)
+print("_"*60)
+zw("czekoladek")
+
+print("_"*60)
+
+
+@startstop
+def dmuchanie(czego):
+    print(f"dmuchanie {czego} na urodzinowym torcie")
+
+dmuchanie("świeczek")
+print("_"*60)
+@startstop
+def f(x):
+    print(2*x)
+
+def g(x):
+    return x-3
+
+f(99)
+print("_"*60)
+print(g(0))
+
+#dekorator policz
+
+def policz(funkcja):
+    def wrapper(*args):
+        print("podstawienie funkcji f(x) i pierwiastek z jej wyniku")
+        print(f"wynik: {math.sqrt(funkcja(*args))}")
+    return wrapper
+
+@policz
+def suma(a,b):
+    return a + b
+
+@policz
+def podiloczyn(x,y):
+    return 2*x*y
+
+suma(5,6)
+podiloczyn(6,7)
+
