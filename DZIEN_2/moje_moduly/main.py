8f8baa167eec8b49e2a425e779dd683f6c39cce8
albim72 <marcin.albiniak72@gmail.com>
2023-12-28 10:28:39 +0100

    Update main.py

--- DZIEN_2/moje_moduly/main.py
+++ DZIEN_2/moje_moduly/main.py
@@ -2,10 +2,6 @@
 # import dane as dn
 from dane import nrfilii as filia
 from dane import book as bk
-# import dane
-# import dane as dn
-from dane import nrfilii as filia
-from dane import book as bk
 
 from funkcje.bfunkcje import czytaj_liste,czytaj_slownik
 
