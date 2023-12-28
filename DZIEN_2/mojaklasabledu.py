6c8ec19a294a2a83d37a1aafa089116d344cf582
albim72 <marcin.albiniak72@gmail.com>
2023-12-28 11:47:31 +0100

    Update mojaklasabledu.py

--- DZIEN_2/mojaklasabledu.py
+++ DZIEN_2/mojaklasabledu.py
@@ -4,10 +4,24 @@ class MyCustomError(Exception):
             self.message = args[0]
         else:
             self.message = None
-            
+
     def __str__(self):
         print("komunikat błędu!")
         if self.message:
             return f"{self.__class__.__name__} -> {self.message}"
         else:
             return f"{self.__class__.__name__} -> brak wiadmości błędu"
+
+
+def info(n=None):
+    if n:
+        print(f'bardzo ważna wiadmość:{n}')
+    else:
+        raise MyCustomError("brak ważnej wiadomości...")
+
+
+try:
+    info("sdfsfgsdf")
+    info()
+except MyCustomError as mc:
+    print(mc)
